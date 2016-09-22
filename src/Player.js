c58b14e04198a13abf89cad33e4429076bd73c09
Andreas Lundahl <mail@andreaslundahl.com>
2016-09-22 22:45:20 +0200

    Add textures

--- src/Player.js
+++ src/Player.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react'
 import './Player.css'
 
-function Player ({ x, y }) {
+function Player ({ direction, x, y }) {
+  const transform = `rotate(${{NORTH: 0, EAST: 90, SOUTH: 180, WEST: 270}[direction]}deg)`
   const top = `${y * 30}px`
   const left = `${x * 30}px`
   return (
-    <div className='Player' style={{ top, left }} />
+    <div className='Player' style={{ top, left, transform }} />
   )
 }
 
